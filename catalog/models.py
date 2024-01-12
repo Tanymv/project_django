ee875c3056fd3ad8f5b87280155253bd2eb738d5
Tanymv <tanymv2004@gmail.com>
2024-01-12 14:09:08 +0300

    до миграций

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="наименование")
+    description = models.TextField(max_length=150, verbose_name="описание")
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="наименование")
+    description = models.TextField(max_length=150, verbose_name="описание")
+    image = models.ImageField(upload_to="catalog/", **NULLABLE, verbose_name="изображение")
+    category = models.CharField(max_length=150, verbose_name="категория")
+    price = models.IntegerField(max_length=150, verbose_name="цена за штуку")
+    date_creation = models.DateTimeField(**NULLABLE, verbose_name="дата создания")
+    last_modified_date = models.DateTimeField(**NULLABLE, verbose_name="дата последнего изменения")
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты'  # Настройка для наименования набора объектов
--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_django',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
